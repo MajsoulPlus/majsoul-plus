9636efe66df7d65ba3c3fe4f70abd59f566324a9
Handle <mail@bysb.net>
2018-12-06 03:29:48 +0800

    将游戏本地服务器地址移动到 ./game/

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = express()
 const Util = require('./Util.js')
 const configs = require('./configs')
 
-server.get('*', Util.processRequest)
+server.get('/game', Util.processRequest)
 
 server.listen(configs.PORT, () => {
   console.log(`服务器已开始监听 ${configs.PORT} 端口`)
