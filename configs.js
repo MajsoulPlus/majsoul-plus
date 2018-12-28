a5bd7457f81be7342498f53384d6add3f494e034
aoarashi1988 <dodolo321@sina.com>
2018-12-28 21:59:10 +0800

    将window的配置剥离到configs中

--- configs.js
+++ configs.js
@@ -7,6 +7,30 @@ const CONFIGS = {
   LOCAL_DIR: '/static',
   MODS_DIR: '/mod',
   PLUGINS_DIR: '/plugins',
-  EXECUTE_DIR: '/execute'
+  EXECUTE_DIR: '/execute',
+  GAME_WINDOW_CONFIG: {
+    width: 960 + 16,
+    height: 540 + 39,
+    frame: true,
+    resizable: true,
+    backgroundColor: '#000000',
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: false
+    // plugins: true
+    }
+  },
+  MANAGER_WINDOW_CONFIG: {
+    width: 960 + 16,
+    height: 540 + 39,
+    frame: true,
+    resizable: true,
+    backgroundColor: '#FFFFFF',
+    webPreferences: {
+      webSecurity: false,
+      allowRunningInsecureContent: true
+    },
+    title: '雀魂Plus - 扩展资源管理器'
+  }
 }
 module.exports = CONFIGS
