3414df50ef72ff12334bc291063e432078e89cae
Handle <mail@bysb.net>
2018-12-29 13:34:56 +0800

    为插件的entry属性提供了一个默认值

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ server.get('*', Util.processRequest)
 electronApp.commandLine.appendSwitch('ignore-certificate-errors')
 
 // 当所有窗口被关闭了，退出。
-electronApp.on('window-all-closed', function () {
+electronApp.on('window-all-closed', function() {
   // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
   // 应用会保持活动状态
   if (process.platform != 'darwin') {
@@ -43,7 +43,7 @@ electronApp.on(
 
 const windowControl = {
   windowMap: {},
-  _getGameWindowTitle: function () {
+  _getGameWindowTitle: function() {
     const titles = [
       {
         text:
@@ -51,13 +51,11 @@ const windowControl = {
         weight: 50
       },
       {
-        text:
-          '雀魂Plus - 扩展插件可能会损害游戏开发者利益，请尽可能支持付费',
+        text: '雀魂Plus - 扩展插件可能会损害游戏开发者利益，请尽可能支持付费',
         weight: 50
       },
       {
-        text:
-          '雀魂Plus - 在使用插件的同时，也不要忘记给别人推荐这款游戏哦',
+        text: '雀魂Plus - 在使用插件的同时，也不要忘记给别人推荐这款游戏哦',
         weight: 50
       },
       {
@@ -83,7 +81,7 @@ const windowControl = {
     return titles[index].text
   },
 
-  _getExcuteScripts: function () {
+  _getExcuteScripts: function() {
     const executeRootDir = path.join(__dirname, configs.EXECUTE_DIR)
     let executeScripts
     try {
@@ -96,7 +94,11 @@ const windowControl = {
     return executeScripts
   },
 
-  _getExcuteCode: function (executeScript) {
+  _getExcuteCode: function(executeScript) {
+    let codeEntry = executeScript.entry
+    if (!codeEntry) {
+      codeEntry = 'script.js'
+    }
     let code = fs
       .readFileSync(path.join(executeScript.filesDir, executeScript.entry))
       .toString('utf-8')
@@ -109,7 +111,7 @@ const windowControl = {
     return code
   },
 
-  _excute: function (gameWindow) {
+  _excute: function(gameWindow) {
     const executeScripts = windowControl._getExcuteScripts()
     executeScripts.forEach(executeScript => {
       console.log('Hack加载 ' + executeScript.name)
@@ -120,54 +122,66 @@ const windowControl = {
 
   _getLocalUrlWithParams(url) {
     if (url.includes('?')) {
-      return `https://localhost:${configs.SERVER_PORT}/0/${url.substring(url.indexOf('?S'))}`
+      return `https://localhost:${configs.SERVER_PORT}/0/${url.substring(
+        url.indexOf('?S')
+      )}`
     }
     return `https://localhost:${configs.SERVER_PORT}/0/`
   },
 
-  _redirectGameWindow: function (url, gameWindow) {
+  _redirectGameWindow: function(url, gameWindow) {
     if (url.startsWith(configs.REMOTE_DOMAIN) && url.includes('?')) {
       const localUrl = windowControl._getLocalUrlWithParams(url)
       gameWindow.loadURL(localUrl)
     }
   },
 
-  electronReady: function () {
+  electronReady: function() {
     return new Promise(resolve => electronApp.once('ready', resolve))
   },
 
-  initLocalMirrorServer: function (sererHttps, port) {
+  initLocalMirrorServer: function(sererHttps, port) {
     return new Promise(resolve => sererHttps.listen(port, resolve))
   },
 
-  initManagerWindow: function (managerWindowConfig) {
+  initManagerWindow: function(managerWindowConfig) {
     const managerWindow = new BrowserWindow(managerWindowConfig)
     managerWindow.on('page-title-updated', evt => evt.preventDefault())
     managerWindow.loadURL(path.join(__dirname, '/manager/index.html'))
     windowControl.windowMap['manager'] = managerWindow
   },
 
-  initGameWindow: function (gameWindowConfig) {
-    const config = { ...gameWindowConfig, title: windowControl._getGameWindowTitle() }
+  initGameWindow: function(gameWindowConfig) {
+    const config = {
+      ...gameWindowConfig,
+      title: windowControl._getGameWindowTitle()
+    }
     const gameWindow = new BrowserWindow(config)
     gameWindow.on('page-title-updated', evt => evt.preventDefault())
-    gameWindow.webContents.on('did-finish-load', () => windowControl._excute(gameWindow))
-    gameWindow.webContents.on('crashed', evt => console.log('web contents crashed'))
+    gameWindow.webContents.on('did-finish-load', () =>
+      windowControl._excute(gameWindow)
+    )
+    gameWindow.webContents.on('crashed', evt =>
+      console.log('web contents crashed')
+    )
     gameWindow.webContents.on('did-navigate', (evt, url) => {
       evt.preventDefault()
       windowControl._redirectGameWindow(url, gameWindow)
     })
-    gameWindow.webContents.on('console-message', (evt, level, msg, line, sourceId) => console.log('Console', msg))
+    gameWindow.webContents.on(
+      'console-message',
+      (evt, level, msg, line, sourceId) => console.log('Console', msg)
+    )
     gameWindow.loadURL(`https://localhost:${configs.SERVER_PORT}/0/`)
     windowControl.windowMap['game'] = gameWindow
   },
 
-  closeManagerWindow: function () {
+  closeManagerWindow: function() {
     const managerWindow = windowControl.windowMap['manager']
     managerWindow && managerWindow.close()
   },
 
-  addAppListener: function () {
+  addAppListener: function() {
     ipcMain.on('application-message', (evt, ...args) => {
       if (args && args.length > 0) {
         switch (args[0]) {
@@ -177,23 +191,22 @@ const windowControl = {
             windowControl.closeManagerWindow()
             break
           default:
-            break;
+            break
         }
       }
     })
   },
 
-  start: function () {
-    windowControl.electronReady()
-      .then(() => {
-        electron.Menu.setApplicationMenu(null)
-        windowControl.addAppListener()
-        windowControl.initManagerWindow(configs.MANAGER_WINDOW_CONFIG)
-      })
+  start: function() {
+    windowControl.electronReady().then(() => {
+      electron.Menu.setApplicationMenu(null)
+      windowControl.addAppListener()
+      windowControl.initManagerWindow(configs.MANAGER_WINDOW_CONFIG)
+    })
   }
 }
 windowControl.start()
 
-process.on('uncaughtException', function (err) {
+process.on('uncaughtException', function(err) {
   console.log(err)
 })
