89987119f8fb0bc2a888e81afa144a553f4b40cc
Handle <mail@bysb.net>
2019-01-28 00:02:29 +0800

    将安装资源函数改为移步，以便后续改造

--- manager/manager.js
+++ manager/manager.js
@@ -358,94 +358,100 @@ const reloadDOM = (executes, mods, tools) => {
 // 安装模组 按钮
 const installMod = document.getElementById('installMod')
 installMod.addEventListener('click', () => {
-  const userChosenPath = dialog.showOpenDialog({
-    title: '选取Mod资源包……',
-    filters: [
-      {
-        name: '雀魂Plus Mod',
-        extensions: ['mspm']
-      },
-      {
-        name: '所有文件',
-        extensions: ['*']
-      }
-    ]
-  })
-  if (userChosenPath && userChosenPath[0]) {
-    userChosenPath.forEach(chosenPath => {
-      const unzip = new AdmZip(chosenPath)
-      unzip.extractAllToAsync(modRootDir, true, err => {
-        if (err) {
-          alert('安装失败！\n错误信息如下:\n' + err)
-        } else {
-          alert('安装成功！')
-          refreshFunction()
+  dialog.showOpenDialog(
+    {
+      title: '选取Mod资源包……',
+      filters: [
+        {
+          name: '雀魂Plus Mod',
+          extensions: ['mspm']
+        },
+        {
+          name: '所有文件',
+          extensions: ['*']
         }
-      })
-    })
-  }
+      ]
+    },
+    filename => {
+      if (filename) {
+        const unzip = new AdmZip(filename)
+        unzip.extractAllToAsync(modRootDir, true, err => {
+          if (err) {
+            alert('安装失败！\n错误信息如下:\n' + err)
+          } else {
+            alert('安装成功！')
+            refreshFunction()
+          }
+        })
+      }
+    }
+  )
 })
 
 // 安装插件 按钮
 const installExecute = document.getElementById('installExecute')
 installExecute.addEventListener('click', () => {
-  const userChosenPath = dialog.showOpenDialog({
-    title: '选取插件资源包……',
-    filters: [
-      {
-        name: '雀魂Plus插件',
-        extensions: ['mspe']
-      },
-      {
-        name: '所有文件',
-        extensions: ['*']
-      }
-    ]
-  })
-  if (userChosenPath && userChosenPath[0]) {
-    userChosenPath.forEach(chosenPath => {
-      const unzip = new AdmZip(chosenPath)
-      unzip.extractAllToAsync(executeRootDir, true, err => {
-        if (err) {
-          alert('安装失败！\n错误信息如下:\n' + err)
-        } else {
-          alert('安装成功！')
-          refreshFunction()
+  dialog.showOpenDialog(
+    {
+      title: '选取插件资源包……',
+      filters: [
+        {
+          name: '雀魂Plus插件',
+          extensions: ['mspe']
+        },
+        {
+          name: '所有文件',
+          extensions: ['*']
         }
-      })
-    })
-  }
+      ]
+    },
+    filename => {
+      if (filename) {
+        const unzip = new AdmZip(filename)
+        unzip.extractAllToAsync(executeRootDir, true, err => {
+          if (err) {
+            alert('安装失败！\n错误信息如下:\n' + err)
+          } else {
+            alert('安装成功！')
+            refreshFunction()
+          }
+        })
+      }
+    }
+  )
 })
 
 // 安装工具 按钮
 const installTool = document.getElementById('installTool')
 installTool.addEventListener('click', () => {
-  const userChosenPath = dialog.showOpenDialog({
-    title: '选取工具资源包……',
-    filters: [
-      {
-        name: '雀魂Plus工具',
-        extensions: ['mspt']
-      },
-      {
-        name: '所有文件',
-        extensions: ['*']
-      }
-    ]
-  })
-  if (userChosenPath && userChosenPath[0]) {
-    userChosenPath.forEach(chosenPath => {
-      const unzip = new AdmZip(chosenPath)
-      unzip.extractAllToAsync(toolsRootDir, true, err => {
-        if (err) {
-          alert('安装失败！\n错误信息如下:\n' + err)
-        } else {
-          alert('安装成功！')
-          refreshFunction()
+  dialog.showOpenDialog(
+    {
+      title: '选取工具资源包……',
+      filters: [
+        {
+          name: '雀魂Plus工具',
+          extensions: ['mspt']
+        },
+        {
+          name: '所有文件',
+          extensions: ['*']
         }
-      })
-    })
-  }
+      ]
+    },
+    filename => {
+      if (filename) {
+        const unzip = new AdmZip(filename)
+        unzip.extractAllToAsync(toolsRootDir, true, err => {
+          if (err) {
+            alert('安装失败！\n错误信息如下:\n' + err)
+          } else {
+            alert('安装成功！')
+            refreshFunction()
+          }
+        })
+      }
+    }
+  )
 })
 
 // 记录编辑状态使用的变量
