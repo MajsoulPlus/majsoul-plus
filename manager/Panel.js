a022e5df426df146ff4210572675ed5425d1cc7b
aoarashi1988 <dodolo321@sina.com>
2019-02-04 23:11:48 +0800

    调整缩进

--- manager/Panel.js
+++ manager/Panel.js
@@ -1,37 +1,37 @@
 class Panel {
-    constructor(){
-        this.activeIndex = 0
-        this.panels = []
-    }
+  constructor(){
+    this.activeIndex = 0
+    this.panels = []
+  }
 
-    _handleClick(index){
-        this.activeIndex = index
-        this.render()
-    }
+  _handleClick(index){
+    this.activeIndex = index
+    this.render()
+  }
 
-    init(){
-        this.panels = Array.from(document.querySelectorAll('.left-pannel ul li'))
-        panels.forEach((panel, index) => {
-            panel.addEventListener('click', () => this._handleClick(index))
-        })
-        this.render()
-    }
+  init(){
+    this.panels = Array.from(document.querySelectorAll('.left-pannel ul li'))
+    this.panels.forEach((panel, index) => {
+      panel.addEventListener('click', () => this._handleClick(index))
+    })
+    this.render()
+  }
 
-    render(){
-        if (this.panels.length) {
-            const currentPanel = this.panels[this.activeIndex]
-            const activeSection = sections.find(section => section.dataset.name === currentPanel.dataset.target)
-            const sections = Array.from(document.getElementsByTagName('section'))
-            sections.forEach(section => {
-                section.className = ''
-            })
-            activeSection.className = 'show'
-            this.panels.forEach(panel => {
-                panel.className = ''
-            })
-            currentPanel.className = 'active'
-        }
+  render(){
+    if (this.panels.length) {
+      const currentPanel = this.panels[this.activeIndex]
+      const activeSection = sections.find(section => section.dataset.name === currentPanel.dataset.target)
+      const sections = Array.from(document.getElementsByTagName('section'))
+      sections.forEach(section => {
+        section.className = ''
+      })
+      activeSection.className = 'show'
+      this.panels.forEach(panel => {
+        panel.className = ''
+      })
+      currentPanel.className = 'active'
     }
+  }
 }
 
 module.exports = new Panel()
