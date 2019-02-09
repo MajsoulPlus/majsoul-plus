cdf55fbcde1c1dd634d8db21e7203202c994e7e5
Handle <mail@bysb.net>
2019-02-10 01:18:41 +0800

    完善i18n思路

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,19 +1,10 @@
-const fs = require('fs')
-const defautLang = 'zh_CN'
-const i18n = new Proxy(
-  {},
-  {
-    get: key => {
-      if (this[key]) {
-        return this[key]
-      }
-      try {
-        fs.statSync(key)
-        this[key] = new Lang(key)
-        return this[key]
-      } catch (error) {
-        return this[defautLang]
-      }
-    }
-  }
-)
+const path = require('path')
+class i18n {
+  constructor({
+    directory = path.join(__dirname, 'locales'),
+    actives = ['zh_CN'],
+    defaultLocale = 'zh_CN',
+    autoReload = false
+  } = {}) {}
+}
+module.exports = i18n
