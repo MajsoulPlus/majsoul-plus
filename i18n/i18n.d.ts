56a507d97c23ee4e93c070dd2227928d27797ec7
Handle <mail@bysb.net>
2019-02-21 14:39:18 +0800

    完善了 i18n 的 .d.ts 文件

--- i18n/i18n.d.ts
+++ i18n/i18n.d.ts
@@ -1,10 +1,18 @@
 /// <reference types="node" />
 
-interface Locale extends Function<string> {
-
+/**
+ * 本地化，单条语句实例对象
+ */
+declare function LocaleObject(...params: string[]): string
+/**
+ * 本地化，单条语句实例对象
+ */
+declare interface LocaleObject {
+  (...params: string[]): string
+  [localeKey: string]: LocaleObject
 }
 
-class i18n {
+declare class i18n {
   /**
    * 构造函数
    */
@@ -12,19 +20,19 @@ class i18n {
   /**
      * 文件夹路径
      */
-  directory?: string = './locales'
+  directory?: string
   /**
      * 语言偏好列表，越靠前越优先
      */
-  actives?: string[] = []
+  actives?: string[]
   /**
      * 无值时的默认语言
      */
-  defaultLocale?: string = 'en-US'
+  defaultLocale?: string
   /**
      * 监听文件修改，自动更新翻译
      */
-  autoReload?: boolean = false
+  autoReload?: boolean
   })
   /**
    * 解绑指定DOM元素的所有绑定
@@ -33,7 +41,7 @@ class i18n {
   /**
    * 绑定一条翻译到指定DOM元素的 innerText
    */
-  bindElementText (
+  bindElementText(
     /**
      * 一个Locale函数对象
      */
@@ -46,8 +54,26 @@ class i18n {
      * Locale参数
      */
     ...args: string[]
-  ) {
-    return this._bindElement(locale, htmlElement, 'text', ...args)
-  }
+  ): void
+  /**
+   * 绑定一条翻译到指定DOM元素的 innerHTML
+   */
+  bindElementHTML(
+    /**
+     * 一个Locale函数对象
+     */
+    locale: LocaleObject,
+    /**
+     * HTMLElement
+     */
+    htmlElement: HTMLElement,
+    /**
+     * Locale参数
+     */
+    ...args: string[]
+  ): void
+  get text(): LocaleObject
+  get t(): LocaleObject
+  get _(): LocaleObject
 }
 export = i18n
--- i18nInstance.js
+++ i18nInstance.js
@@ -1,7 +1,10 @@
 if (!global._i18nInstance) {
   const I18n = require('./i18n/i18n')
-  global._i18nInstance = new I18n({
+  const i18nInstance = new I18n({
     autoReload: process.env.NODE_ENV === 'development'
   })
+  global._i18nInstance = i18nInstance
+  module.exports = i18nInstance
+} else {
+  module.exports = global._i18nInstance
 }
-module.exports = global._i18nInstance
