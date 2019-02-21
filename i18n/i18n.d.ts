c77b89e809a7c2cee21cfb8e37bebf7458e801fe
Handle <mail@bysb.net>
2019-02-21 14:56:35 +0800

    i18n .d.ts 文件超完善

--- i18n/i18n.d.ts
+++ i18n/i18n.d.ts
@@ -1,15 +1,72 @@
 /// <reference types="node" />
 
 /**
- * 本地化，单条语句实例对象
+ * 本地化，单条语句翻译对象
+ * @param params 格式化参数，会自动替换文本中的 $1、$2
  */
-declare function LocaleObject(...params: string[]): string
+declare function Locale(
+  /**
+   * 格式化参数，会自动替换文本中的 $1、$2
+   */
+  ...params: string[]
+): string
 /**
- * 本地化，单条语句实例对象
+ * 本地化，单条语句翻译对象
  */
-declare interface LocaleObject {
-  (...params: string[]): string
-  [localeKey: string]: LocaleObject
+declare interface Locale {
+  /**
+   * 格式化该键对应的翻译文本并返回
+   */
+  (
+    /**
+     * 格式化参数，会自动替换文本中的 $1、$2
+     */
+    ...params: string[]
+  ): string
+  /**
+   * 文本的键
+   */
+  [localeKey: string]: Locale
+  /**
+   * 绑定该条翻译到指定DOM元素的 innerText
+   * @param htmlElement 要绑定的 HTMLElement 元素
+   * @param params 格式化参数，会自动替换文本中的 $1、$2
+   */
+  renderAsText(
+    /**
+     * 要绑定的 HTMLElement 元素
+     */
+    htmlElement: HTMLElement,
+    /**
+     * 格式化参数，会自动替换文本中的 $1、$2
+     */
+    ...params: string[]
+  ): void
+  /**
+   * 绑定该条翻译到指定DOM元素的 innerHTML
+   * @param htmlElement 要绑定的 HTMLElement 元素
+   * @param params 格式化参数，会自动替换文本中的 $1、$2
+   */
+  renderAsHTML(
+    /**
+     * 要绑定的 HTMLElement 元素
+     */
+    htmlElement: HTMLElement,
+    /**
+     * 格式化参数，会自动替换文本中的 $1、$2
+     */
+    ...params: string[]
+  ): void
+}
+
+/**
+ * 本地化，根级翻译文本对象
+ */
+declare interface LocaleMain {
+  /**
+   * 翻译文本的键
+   */
+  [localeKey: string]: Locale
 }
 
 declare class i18n {
@@ -18,7 +75,7 @@ declare class i18n {
    */
   constructor(i18nInitConfigs?: {
   /**
-     * 文件夹路径
+     * 翻译文件所在的文件夹路径
      */
   directory?: string
   /**
@@ -26,54 +83,75 @@ declare class i18n {
      */
   actives?: string[]
   /**
-     * 无值时的默认语言
+     * 在找不到翻译文本时的默认语言
      */
   defaultLocale?: string
   /**
-     * 监听文件修改，自动更新翻译
+     * 是否监听文件修改，以自动更新翻译
      */
   autoReload?: boolean
   })
   /**
-   * 解绑指定DOM元素的所有绑定
+   * 解绑指定DOM元素的所有绑定的翻译
+   * @param htmlElement 要解绑的 HTMLElement
    */
-  unbindElement(htmlElement: HTMLElement): void
+  unbindElement(
+    /**
+     * @param htmlElement 要解绑的 HTMLElement
+     */
+    htmlElement: HTMLElement
+  ): void
   /**
    * 绑定一条翻译到指定DOM元素的 innerText
+   * @param locale 一个要绑定的单条语句翻译对象
+   * @param htmlElement 要绑定要的 HTMLElement
+   * @param params 格式化参数，会自动替换文本中的 $1、$2
    */
   bindElementText(
     /**
-     * 一个Locale函数对象
+     * 一个要绑定的单条语句翻译对象
      */
-    locale: LocaleObject,
+    locale: Locale,
     /**
-     * HTMLElement
+     * 要绑定要的 HTMLElement
      */
     htmlElement: HTMLElement,
     /**
-     * Locale参数
+     * 格式化参数，会自动替换文本中的 $1、$2
      */
-    ...args: string[]
+    ...params: string[]
   ): void
   /**
    * 绑定一条翻译到指定DOM元素的 innerHTML
+   * @param locale 一个要绑定的单条语句翻译对象
+   * @param htmlElement 要绑定要的 HTMLElement
+   * @param params 格式化参数，会自动替换文本中的 $1、$2
    */
   bindElementHTML(
     /**
-     * 一个Locale函数对象
+     * 一个要绑定的单条语句翻译对象
      */
-    locale: LocaleObject,
+    locale: Locale,
     /**
-     * HTMLElement
+     * HTMLEle要绑定要的 HTMLElementment
      */
     htmlElement: HTMLElement,
     /**
-     * Locale参数
+     * 格式化参数，会自动替换文本中的 $1、$2
      */
-    ...args: string[]
+    ...params: string[]
   ): void
-  get text(): LocaleObject
-  get t(): LocaleObject
-  get _(): LocaleObject
+  /**
+   * 根级翻译文本对象
+   */
+  get text(): LocaleMain
+  /**
+   * 根级翻译文本对象
+   */
+  get t(): LocaleMain
+  /**
+   * 根级翻译文本对象
+   */
+  get _(): LocaleMain
 }
 export = i18n
