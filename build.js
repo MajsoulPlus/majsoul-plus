0574a02cfc0edab956bbe6563f05fd89816c8ee6
Yesterday17 <t@yesterday17.cn>
2019-04-04 22:21:07 +0800

    将 manager 下的静态文件分离至 assets

--- build.js
+++ build.js
@@ -4,9 +4,8 @@ const dest = "./dist/";
 
 // Copy files
 ncp("./assets", dest, err => (err ? console.error(err) : null));
-ncp("./execute", dest + "execute", err => (err ? console.error(err) : null));
 ncp("./i18n", dest + "i18n", err => (err ? console.error(err) : null));
-ncp("./manager", dest + "manager", err => (err ? console.error(err) : null));
+ncp("./execute", dest + "execute", err => (err ? console.error(err) : null));
 ncp("./mod", dest + "mod", err => (err ? console.error(err) : null));
 ncp("./tool", dest + "tool", err => (err ? console.error(err) : null));
 
