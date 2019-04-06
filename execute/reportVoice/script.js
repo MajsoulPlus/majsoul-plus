d0f9625c12735680f7c3a73e257395755025a3c8
Handle <mail@bysb.net>
2019-04-06 15:36:54 +0800

    修正：修正了自带的强制报番插件无法正常工作的问题

--- execute/reportVoice/script.js
+++ execute/reportVoice/script.js
@@ -1,47 +1,6 @@
-// Hack 开启报番型，作者 aoarashi1988，Handle修改
+// Hack 开启报番型，作者 Majsoul Plus Team
 if (game) {
-  game.Tools.get_chara_audio = function(e, t) {
-    if (t && '' != t) {
-      var charid = e.charid,
-        chara = cfg.item_definition.character.get(charid)
-      if (!chara) return null
-      for (
-        var a = 6, r = cfg.voice.sound.findGroup(chara.sound), o = [], s = 0;
-        s < r.length;
-        s++
-      )
-        r[s].type == t && r[s].level_limit <= a && o.push(s)
-      if (0 == o.length) return null
-      var h = o[Math.floor(Math.random() * o.length)]
-      return {
-        path: r[h].path,
-        volume: chara.sound_volume,
-        time_length: r[h].time_length
-      }
-    }
-  }
-  view.AudioMgr.PlayCharactorSound = function(e, t, i) {
-    var n = e.charid,
-      a = cfg.item_definition.character.get(n)
-    if (!a) return null
-    for (
-      var r = /* e.level */ 6,
-        o = cfg.voice.sound.findGroup(a.sound),
-        s = [],
-        h = 0;
-      h < o.length;
-      h++
-    )
-      o[h].type == t && o[h].level_limit <= r && s.push(h)
-    if (0 == s.length) return null
-    var l = s[Math.floor(Math.random() * s.length)]
-    return {
-      words: o[l].words,
-      sound: this.PlaySound(o[l].path, a.sound_volume, i)
-    }
-  }
-
-  requestAnimationFrame(function autoRun() {
+  requestAnimationFrame(function autoRun () {
     try {
       const arrBackup = cfg.voice.sound.groups_
       if (!arrBackup || arrBackup.length === 0) {
