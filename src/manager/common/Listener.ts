9e2e9617192797ea9622c098e32dbdb8ae18e8a4
Yesterday17 <t@yesterday17.cn>
2019-04-29 15:16:49 +0800

    重构 Listener

---
+++ src/manager/common/Listener.ts
@@ -0,0 +1,43 @@
+interface HandlerItem {
+  event: string
+  handle: Function
+}
+
+class Listener {
+  private handles: HandlerItem[] = []
+
+  on(event: string, handle: Function) {
+    for (const element of this.handles) {
+      if (element.event === event && element.handle === handle) {
+        return
+      }
+    }
+    this.handles.push({
+      event,
+      handle
+    })
+  }
+
+  off(event: string, handle?: Function) {
+    if (handle) {
+      this.handles.forEach((element, index) => {
+        if (element.event === event && element.handle === handle) {
+          this.handles.splice(index, 1)
+        }
+      })
+    } else {
+      this.handles = this.handles.filter(item => item.event !== event)
+    }
+  }
+
+  emit(event: string, ...args) {
+    for (const element of this.handles) {
+      if (element.event === event) {
+        element.handle(...args)
+      }
+    }
+  }
+}
+
+// TODO: 禁止导出分配
+export = Listener
