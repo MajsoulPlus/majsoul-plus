f2c1238c06a9eef37adc9d729ca85db29cd77063
Yesterday17 <t@yesterday17.cn>
2019-07-08 14:09:41 +0800

    默认使用 appDataDir 存储 static 的内容(#46) Windows 下为 %appdata%/Majsoul_Plus/static *nix下为~/.local/Majsoul_Plus/static

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import {
   getRemoteSource,
   writeFile
 } from './utils-refactor'
+import { GlobalPath, appDataDir } from './global'
 
 // tslint:disable-next-line
 export const Server = new Koa()
@@ -28,29 +29,38 @@ Server.use(async (ctx, next) => {
   const originalUrl = ctx.request.originalUrl
   const encrypt = isEncryptRes(originalUrl)
   const isRoutePath = isPath(originalUrl)
-  const localURI = getLocalURI(originalUrl, isRoutePath)
+  const localURI = getLocalURI(originalUrl, isRoutePath, path.join(appDataDir, GlobalPath.LocalDir))
 
   let allData: string | Buffer
 
-  try {
-    const { res: result, data } = await getRemoteSource(
-      originalUrl,
-      encrypt && !isPath
-    )
-    ctx.res.statusCode = result.statusCode
-    if (!isPath) {
-      writeFile(localURI, data)
+  if (!isRoutePath && fs.existsSync(localURI)) {
+    try {
+      allData = await readFile(localURI)
+    } catch (e) {
+      console.error(e)
+      return
     }
-    allData = data
-  } catch ({ res: result, data }) {
-    ctx.res.statusCode = result.statusCode
-    return
+  } else {
+    try {
+      const { res: result, data } = await getRemoteSource(
+        originalUrl,
+        encrypt && !isRoutePath
+      )
+      ctx.res.statusCode = result.statusCode
+      if (!isRoutePath) {
+        writeFile(localURI, data)
+      }
+      allData = data
+
+    } catch ({ res: result, data }) {
+      ctx.res.statusCode = result.statusCode
+      return
+    }
   }
 
   let sendData = isRoutePath
     ? encodeData(allData).toString('utf-8')
     : encodeData(allData)
-  console.log(sendData)
   if (encrypt) {
     sendData = XOR(sendData as Buffer)
   }
