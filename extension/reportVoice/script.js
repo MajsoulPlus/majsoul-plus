9ee1d69df1d1a69b95e5e0e630fbe0cad26662a9
Yesterday17 <t@yesterday17.cn>
2019-10-01 20:29:10 +0800

    升级报菜名

--- extension/reportVoice/script.js
+++ extension/reportVoice/script.js
@@ -1,21 +1,25 @@
 // Hack 开启报番型，作者 Majsoul Plus Team
-if (game) {
-  requestAnimationFrame(function autoRun () {
-    try {
-      const arrBackup = cfg.voice.sound.groups_
-      if (!arrBackup || arrBackup.length === 0) {
-        throw new Error()
-      }
-      console.log('Hacked所有语音')
-      Object.entries(cfg.voice.sound.groups_).forEach(
-        ([soundID, soundGroup]) => {
-          soundGroup.forEach((soundObject, index) => {
-            soundObject.level_limit = 0
-          })
-        }
-      )
-    } catch (error) {
-      requestAnimationFrame(autoRun)
+
+function autoRun() {
+  try {
+    const arrBackup = cfg.voice.sound.groups_
+    if (!arrBackup || arrBackup.length === 0) {
+      throw new Error()
     }
-  })
+    Object.entries(cfg.voice.sound.groups_).forEach(([soundID, soundGroup]) => {
+      soundGroup.forEach((soundObject, index) => {
+        soundObject.level_limit = 0
+      })
+    })
+    console.log('Hacked所有语音')
+  } catch (error) {
+    requestAnimationFrame(autoRun)
+  }
 }
+
+const reportVoice = setInterval(() => {
+  if (game) {
+    clearInterval(reportVoice)
+    requestAnimationFrame(autoRun)
+  }
+}, 1000)
