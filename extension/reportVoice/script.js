145e718a039fcdb9027332d15d6c9a2d2198e717
Handle <mail@bysb.net>
2020-06-10 02:16:21 +0800

    🦄refactor(插件 > 报菜名): 现已重写 旧的报菜名已经无法稳定保障婚后语音可以正常播放，现在新的报菜名使用了一种需要稍高资源的新的Hack方式，可以有效保障角色语音能够稳定Hack

--- extension/reportVoice/script.js
+++ extension/reportVoice/script.js
@@ -1,16 +1,36 @@
 // Hack 开启报番型，作者 Majsoul Plus Team
 
-function autoRun() {
-  try {
-    const arrBackup = cfg.voice.sound.rows_
-    if (!arrBackup || arrBackup.length === 0) {
-      throw new Error()
-    }
-    cfg.voice.sound.rows_.forEach(sound=>{
+const hackVoice = () => {
+  if (cfg && cfg.voice && cfg.voice.sound) {
+    cfg.voice.sound.rows_.forEach(sound => {
       sound.level_limit = 0
       sound.bond_limit = 0
     })
-    console.log('Hacked所有语音')
+  }
+}
+
+const autoRun = () => {
+  try {
+    if (cfg.voice.sound) {
+      const raw = cfg.voice.sound
+      const proxy = new Proxy(raw, {
+        get(target, key) {
+          console.log('Get', key)
+          setTimeout(hackVoice, 0)
+          return target[key]
+        },
+        set(target, key, value) {
+          console.log('Set', key, value)
+          target[key] = value
+          setTimeout(hackVoice, 0)
+          return true
+        }
+      })
+      cfg.voice.sound = proxy
+      console.log('Hacked所有语音')
+    } else {
+      throw new Error()
+    }
   } catch (error) {
     requestAnimationFrame(autoRun)
   }
